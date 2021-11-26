8c055c1f858a04403e9fc7150aba4923d82a2ed2
Ajay Kumawat <ajaykumavat@nimapinfotech.com>
2021-11-26 14:04:29 +0530

    Add a generic Repository (Interface + Implementation) inside Shop.Database project

---
+++ Shop.Database/Infrastructure/Repository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Database.Infrastructure
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected ShopDbContext _context;
+        public Repository(ShopDbContext context)
+        {
+            _context = context;
+        }
+
+        public virtual void Add(T entity)
+        {
+            _context.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public virtual void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public virtual async Task<IEnumerable<T>> Get()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public virtual async Task<T> GetById(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public virtual void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.Set<T>().Update(entity);
+            _context.SaveChangesAsync();
+        }
+    }
+}
