856e6b12455ca879739c505c37e4a7dcf66f6c9f
Ajay Kumawat <ajaykumavat@nimapinfotech.com>
2021-12-12 14:58:22 +0530

    Add a new class UserLoginDTO

---
+++ Shop.DTO/AccountDTOs/UserLoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.DTO.AccountDTOs
+{
+    public class UserLoginDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
